e8fec86316da7a458434257dfc13e12fa20b9769
Jack Mitchell <mitcjack@shernet.sheridancollege.ca>
2024-03-21 09:58:59 -0400

    task 1 complete: dashing for theif made gooder

--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -5,24 +5,42 @@ using UnityEngine;
 public class Thief : Villager
 {
     public GameObject knifePrefab;
-    public Transform spawnPoint;
-    float knifeCount = 1;
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
+    Coroutine dashing;
+    
+    protected override void Attack()
+    {
+        if (dashing != null)
+        {
+            StopCoroutine(dashing);
+        }
+        dashing = StartCoroutine(Dash());
+    }
+    IEnumerator Dash()
+    {
+        //dash towards mouse
+        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        speed = 7;
+        while (speed > 3)
+        {
+            yield return null;
+        }
+
+        base.Attack();
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(knifePrefab, spawnPoint1.position, spawnPoint1.rotation);
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(knifePrefab, spawnPoint2.position, spawnPoint2.rotation);
+    }
 
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
     }
 
-    protected override void Attack()
+    public override string ToString()
     {
-        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        speed = 5;
-        base.Attack();
-        while (knifeCount <= 2)
-        {
-            knifeCount++;
-            Instantiate(knifePrefab, spawnPoint.position, spawnPoint.rotation);
-        }
-        knifeCount = 1;
+        return "it's Theifing time!";
     }
 }
